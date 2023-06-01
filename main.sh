f733e9e8116b12b2f0aef8de2e70bd1fd6b4928c
Liamw3382 <124923465+Liamw3382@users.noreply.github.com>
2023-05-31 20:59:40 -0400

    1 file modified

--- main.sh
+++ main.sh
@@ -18,7 +18,7 @@ emergbukkit="false"
 
 #~#       this code was smashed together by ayunami2000       #~#
 
-eagurl="https://raw.githubusercontent.com/LAX1DUDE/eaglercraft/main/stable-download/stable-download_repl.zip"
+eagurl="https://raw.githubusercontent.com/Liamw3382/stable-download/main/stable-download/stable-download.zip"
 
 echo ensuring old server process is truly closed...
 nginx -s stop -c ~/$REPL_SLUG/nginx.conf -g 'daemon off; pid /tmp/nginx/nginx.pid;' -p /tmp/nginx -e /tmp/nginx/error.log
